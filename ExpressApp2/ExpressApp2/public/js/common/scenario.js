20c45d20d1c3cd83b49eeb97a642a2d8de499ec6
epkimtaiho <epkim@taihoinst.com>
2018-08-23 14:57:28 +0900

    js 갱신
    js 갱신

--- ExpressApp2/ExpressApp2/public/js/common/scenario.js
+++ ExpressApp2/ExpressApp2/public/js/common/scenario.js
@@ -119,24 +119,34 @@ $(function () {
 
     /////////////////////////// 버튼 추가/삭제 ///////////////////////////////
     var btnCnt = 1;
+    var btnNum_temp = "";
+    var btnNum_list = "";   //버튼의 번호 목록
     $('.addBtn').click(function () {
         var divCnt = $(".btnArea > div").length;
         if (divCnt < 4) {
             $(".btnArea > div").add(
                 "<div class='dlgBtn button" + btnCnt + "'>" +
-                    "<div>버튼" + btnCnt + "<span class='removeBtn'>삭제</span></div>" +
-                    "<div class='btnMenu1'>버튼명" +
-                        "<input type='text' class='btnTitle' placeholder='버튼명을 입력하세요.'>" +
-                    "</div > " +
-                    "<div class='btnMenu2' name='button" + btnCnt + "'>버튼답변" +
-                        "<input type='text' class='btnAnswer' placeholder='버튼답변을 입력하세요.'>" +
-                    "</div > " +
-                    "<div class='btnMenu3'>버튼타입" +
-                        "<label class='cp'><input type='radio' class='btnType' name='btnType" + btnCnt + "' value='imBack' checked> imBack</label>" +
-                        "<label class='cp'><input type='radio' class='btnType' name='btnType" + btnCnt + "' value='openUrl'> openUrl</label>" +
-                    "</div > " +
+                    "<div>버튼" + btnCnt + "<span class='removeBtn' alt=" + btnCnt + ">삭제</span></div>" +
+
+                    "<div class='btnMenu1'>버튼명</div>" +
+                    "<input type='text' id='btnTitle" + btnCnt + "' class='btnTitle' onkeyup='btnTitleKeyup(this, " + btnCnt + ")' placeholder='버튼명을 입력하세요.'>" +
+
+                    "<div class='btnMenu2' name='button" + btnCnt + "'>버튼답변</div>" +
+                    "<input type='text' id='btnAnswer" + btnCnt + "' class='btnAnswer' onkeyup='btnAnswerKeyup(this, " + btnCnt + ")' placeholder='버튼답변을 입력하세요.'>" +
+
+                    "<div class='btnMenu3'>버튼타입</div> " +
+                    "<label class='cp'><input type='radio' class='btnType' name='btnType" + btnCnt + "' value='imBack' checked> imBack</label>" +
+                    "<label class='cp'><input type='radio' class='btnType' name='btnType" + btnCnt + "' value='openUrl'> openUrl</label>" +
                 "</div>"
             ).appendTo(".btnArea");
+
+            btnNum_temp = btnCnt.toString();
+            if (divCnt == 0) {
+                btnNum_list = btnNum_temp;
+            } else {
+                btnNum_list = btnNum_list + ',' +btnNum_temp;
+            }
+
             btnCnt++;
             
             //다중을 체크하고 disabled 시킨다.
@@ -150,6 +160,20 @@ $(function () {
         }
     });
     $(document).on('click', '.removeBtn', function () {
+        var btnNum = btnNum_list.split(',');
+        btnNum_list = "";
+        var _tempFlag = 0;
+        for (var i = 0; i < btnNum.length; i++) {
+            if (btnNum[i] != $(this).attr('alt')) {
+                if (_tempFlag == 0) {
+                    btnNum_list = btnNum[i];
+                    _tempFlag = 1;
+                } else {
+                    btnNum_list = btnNum_list + ',' + btnNum[i];
+                }
+            }
+        }
+
         var clickBtn = $(this).parent().parent().hasClass('dlgBtn');
         if (clickBtn) {
             $(this).parent().parent().remove();
@@ -164,17 +188,17 @@ $(function () {
         }
     });
     $(document).on('click', '.btnType', function () {
+        var _this = $(this).parent().parent().parent().attr('class');
+        var _thisNum = String(_this).substr(_this.length - 1);
         if ($(this).val() == 'imBack') {
-            var _this = $(this).parent().parent().parent().attr('class');
-            var _thisNum = String(_this).substr(_this.length - 1);
             for (var i = 0; i < $(".btnArea > div").length; i++) {
-                $('.dlgBtn.button' + _thisNum + ' > .btnMenu2').removeClass('dpN')
+                //$('.dlgBtn.button' + _thisNum + ' > .btnMenu2').removeClass('dpN');
+                $('.dlgBtn.button' + _thisNum + ' > .btnMenu2').text('버튼답변');
             }
         } else {
-            var _this = $(this).parent().parent().parent().attr('class');
-            var _thisNum = String(_this).substr(_this.length-1);
             for (var i = 0; i < $(".btnArea > div").length; i++) {
-                $('.dlgBtn.button' + _thisNum+' > .btnMenu2').addClass('dpN')
+                //$('.dlgBtn.button' + _thisNum + ' > .btnMenu2').addClass('dpN');
+                $('.dlgBtn.button' + _thisNum + ' > .btnMenu2').text('URL');
             }
         }
     });
@@ -196,6 +220,7 @@ $(function () {
     });
     
     /////////////////////////// 생성 관련 ///////////////////////////////
+    var board_dlgId = 1;
     $('.createDlg').on('click', function () {
         var valChk = validationChk();
         if (valChk){
@@ -212,25 +237,50 @@ $(function () {
             var board_carousel = $('input:radio[name=cardType]:checked').attr('value');
             var board_parentId = $('.pDlgId').attr('value');
 
-
+            //버튼 갯수만큼 생성
             var btn_title = "";
             var btn_result = "";
             var btn_type = "";
-            var btnCreate = "";
+            var btnCreate_title = "";
+            var btnCreate_answer = "";
+            var btnCreate_type = "";
+            var btnNum = "";
             if (board_btn > 0) {
-                btn_title = $('.btnTitle').attr('value');
-                for (var i= 1; i <= board_btn; i++){
-                    btnCreate = btnCreate + "<div class='board_btn btn"+i+"'>" + btn_title + '"</div>"';
+                //btn_title = $('.btnTitle').attr('value');
+                btnNum = btnNum_list.split(',');
+                for (var i = 1; i <= board_btn; i++){
+                    btn_title = $('#btnTitle' + btnNum[i - 1]).attr('value');
+                    btnCreate_title = btnCreate_title + "<div class='board_btn btn" + i + "'>" + btn_title + "</div>";
+
+                    btn_result = $('#btnAnswer' + btnNum[i - 1]).attr('value');
+                    btnCreate_answer = btnCreate_answer + "<div class='board_btn_answer hidden btn" + i + "' alt=" + btn_result + "></div>";
+
+                    btn_type = $("input:radio[name=btnType" + btnNum[i - 1] + "]:checked").attr('value');
+                    btnCreate_type = btnCreate_type + "<div class='board_btn_type hidden btn" + i + "' alt=" + btn_type + "></div>";
+                }
+            }
+
+            //이미지 유무 판단 생성
+            var imgCreate = "";
+            if (board_type == '3' || board_type == '4') {
+                var ori_path = board_img;
+                board_img = board_img.substr(board_img.length - 4);
+                if (board_img == '.jpg' || board_img == '.png') {
+                    imgCreate = "<div class='board_img img" + board_dlgId + "'><img src='" + ori_path + "'></div>"
+                } else {
+                    imgReset();
                 }
             }
 
             $(".viewArea > div").add(
                 "<div class='dlgBoard'>" +
-                    "<div class='board_img dpN'><img src='" + board_img + "'></div>" +
-                    "<div class='board_title'>" + board_title + "</div>" +
+                    imgCreate +
+                    "<div class='board_title'><b>" + board_title + "</b></div>" +
                     "<div class='board_subTitle'>" + board_subTitle + "</div>" +
                     "<div class='board_text'>" + board_text + "</div>" +
-                    btnCreate +
+                    btnCreate_title +
+                    btnCreate_answer +
+                    btnCreate_type +
                     "<div class='board_dlgId hidden' alt='" + board_dlgId +"'></div>" +
                     "<div class='board_type hidden' alt='" + board_type +"'></div>" +
                     "<div class='board_carousel hidden' alt='" + board_carousel+"'></div>" +
@@ -238,30 +288,43 @@ $(function () {
                 "</div>"
             ).appendTo(".viewArea");
 
-            board_img = board_img.substr(board_img.length - 4);
-            if (board_img == '.jpg' || board_img == '.png') {
-                $('.board_img').removeClass('dpN');
-            } else {
-                $('.board_img').addClass('dpN');
-                imgReset();
-            }
-
             //form 지우기
-            $("input[type=text], textarea").val("");
+            formReset()
+            imgReset();
+
             //초기화후 dlgid랑 parentId search
+            $('.pDlgId').attr('value', board_dlgId);
+            $('.pDlgId').val(board_dlgId);
+            board_dlgId++;
+            $('.dlgId').attr('value', board_dlgId);
+            $('.dlgId').val(board_dlgId);
+            
         }
 
 
     });
 
     $('.cancelDlg').on('click', function () {
-        $("input[type=text], textarea").val("");
+        formReset()
         imgReset();
         //초기화후 dlgid랑 parentId search
     });
 
-    
-    
+    /////////////////////////// board data select & update ///////////////////////////////
+    $(document).on('click', '.dlgBoard', function () {
+
+        alert('준비중입니다.');
+        console.log($(this).children().attr('class'));
+        console.log($(this).children().eq(1).text());
+
+        var board_title = $(this).children().eq(1).text();
+
+
+        //for (var attr in $(this)) {
+        //    console.log($(this)[attr]);
+        //}
+    });
+
 });
 
 /////////////////////////// validation check ///////////////////////////////
@@ -285,4 +348,91 @@ function validationChk() {
     return result;
 }
 
+$(function () {
+    /////////////////////////// 디바이스에 맞게 초기 높이 설정 ///////////////////////////////
+    $('.scenarioArea').css({ 'height': $(document).height() - 20 + 'px' });
+    $('.optionArea').css({ 'height': $(document).height() - 20 + 'px' });
+    $('.viewArea').css({ 'height': $(document).height() - 20 + 'px' });
+    $(window).resize(function () {
+        $('.scenarioArea').css({ 'height': $(document).height() - 20 + 'px' });
+        $('.optionArea').css({ 'height': $(document).height() - 20 + 'px' });
+        $('.viewArea').css({ 'height': $(document).height() - 20 + 'px' });
+    });
 
+    /////////////////////////// input창 value 변경값 ///////////////////////////////
+    $('input[id=dlgTitle]').keyup(function () {
+        $('#dlgTitle').attr('value', $(this).val());
+    });
+    $('input[id=dlgSubTitle]').keyup(function () {
+        $('#dlgSubTitle').attr('value', $(this).val());
+    });
+    $('input[id=dlgText]').keyup(function () {
+        $('#dlgText').attr('value', $(this).val());
+    });
+    $('input[id=input-file]').change(function () {
+        var imgNm = $(this)[0].files[0].name;
+        var realpath = 'C:\\Users\\Taiho_KSO\\Desktop\\';   //절대경로 지정.....
+        realpath = realpath + imgNm;
+        $('#input-file').attr('value', realpath);
+    });
+    $('input[id=dlgUrl]').keyup(function () {
+        $('#dlgUrl').attr('value', $(this).val());
+    });
+
+    /////////////////////////// 파일 업로드 관련 ///////////////////////////////
+    var fileTarget = $('.filebox .upload-hidden');
+    fileTarget.on('change', function () { // 값이 변경되면 
+        if (window.FileReader) { // modern browser 
+            var filename = $(this)[0].files[0].name;
+        } else { // old IE 
+            var filename = $(this).val().split('/').pop().split('\\').pop(); // 파일명만 추출 
+        } // 추출한 파일명 삽입 
+        $(this).siblings('.upload-name').val(filename);
+    });
+
+    //preview image 
+    var imgTarget = $('.preview-image .upload-hidden');
+    imgTarget.on('change', function () {
+        var parent = $(this).parent();
+        parent.children('.upload-display').remove();
+        if (window.FileReader) { //image 파일만 
+            if (!$(this)[0].files[0].type.match(/image\//))
+                return;
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var src = e.target.result;
+                parent.prepend('<div class="upload-display"><div class="upload-thumb-wrap"><img src="' + src + '" class="upload-thumb"></div></div>');
+            }
+            reader.readAsDataURL($(this)[0].files[0]);
+        } else {
+            $(this)[0].select();
+            $(this)[0].blur();
+            var imgSrc = document.selection.createRange().text;
+            parent.prepend('<div class="upload-display"><div class="upload-thumb-wrap"><img class="upload-thumb"></div></div>');
+            var img = $(this).siblings('.upload-display').find('img');
+            img[0].style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(enable='true',sizingMethod='scale',src=\"" + imgSrc + "\")";
+        }
+    });
+});
+
+function formReset() {
+    $("input[type=text], textarea").val("");
+}
+
+function imgReset() {
+    $('.upload-display').remove();
+    $('.upload-hidden').attr('value', '');
+    $('.upload-name').val('파일선택');
+}
+
+function btnTitleKeyup(text, btnNum) {
+    var divCnt = $(".btnArea > div").length;
+    $('#btnTitle' + btnNum).attr('value', text.value);
+    $('#btnTitle' + btnNum).val(text.value);
+}
+
+function btnAnswerKeyup(text, btnNum) {
+    var divCnt = $(".btnArea > div").length;
+    $('#btnAnswer' + btnNum).attr('value', text.value);
+    $('#btnAnswer' + btnNum).val(text.value);
+}
